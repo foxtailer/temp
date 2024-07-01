8f76c3fbacea35a85dc24db4607522fa07001f31
Your Name <you@example.com>
2024-07-01 09:22:12 +0300

    gg

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
     
 
@@ -14,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -32,8 +33,13 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year,
+                            self.publish.month,
+                            self.publish.day,
+                            self.slug])
+                            
     class Meta:
         ordering = ['-publish']
-        indexes = [
-            models.Index(fields=['-publish']),
-        ]
+        indexes = [models.Index(fields=['-publish']),]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
 
-# Create your views here.
+
+def post_list(request):
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,
+                'blog/post/list.html',
+                {'posts': posts}
+            )
+
+
+def post_detail(request, year, month, day, post):
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found.")
+
+    post = get_object_or_404(Post,
+                            slug=post,
+                            publish__year=year,
+                            publish__month=month,
+                            publish__day=day,
+                            status=Post.Status.PUBLISHED)
+
+    return render(request,
+                'blog/post/detail.html',
+                {'post': post})
+
+
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post,
+                            id=post_id,
+                            status=Post.Status.PUBLISHED)
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            # ... отправить электронное письмо
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
